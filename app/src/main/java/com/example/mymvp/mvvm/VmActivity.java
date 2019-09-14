e071bebce7fdb47d36c65056598fb557efb45320
freedomangelly <674919909@qq.com>
2019-09-14 10:16:55 +0800

    创建分支

---
+++ app/src/main/java/com/example/mymvp/mvvm/VmActivity.java
@@ -0,0 +1,20 @@
+package com.example.mymvp.mvvm;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * description:
+ * author: freed on 2019/9/9
+ * email: 674919909@qq.com
+ * version: 1.0
+ */
+public class VmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
